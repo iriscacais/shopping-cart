ed168f1ce699303a46742cb732e02a4264fb9960
iriscacais <mandeparairis@gmail.com>
2022-11-17 16:54:05 -0300

    requisito 4 e 5

--- src/main.js
+++ src/main.js
@@ -4,12 +4,37 @@ import { fetchProductsList } from './helpers/fetchFunctions';
 import { createProductElement } from './helpers/shopFunctions';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
-
 const productList = document.querySelector('.products'); // capturo a section onde quero colocar as informações da API
-const produtos = await fetchProductsList('computador'); // chamo a função fetchProductsList com o argumento computador - aqui retorno a API - o retorno é igual ao search.js que está na pasta mock!
+const mensageLoading = document.createElement('p');
+const mensageError = document.createElement('p');
 
-produtos.forEach((produto) => { // percorro cada elemento do objeto retornado pela API
-  const { id, title, thumbnail, price } = produto; // desestruturo o objeto produto para pegar apenas os elementos que vou apresentar no html. - no caso sao os elementos criados também pela função createProductElemenst
-  const product = createProductElement({ id, title, thumbnail, price }); // chamo a funçao que cria os elementos no html
-  productList.appendChild(product); // faço o appendChild para que as informaçoes apareçam no html!
-});
+function createLoading() {
+  mensageLoading.classList.add('loading');
+  mensageLoading.innerText = 'carregando...';
+  productList.appendChild(mensageLoading);
+}
+
+function removeLoading() {
+  mensageLoading.remove();
+}
+function errorMessage() {
+  mensageError.classList.add('error');
+  mensageError.innerText = 'Algum erro ocorreu, recarregue a página e tente novamente';
+  productList.appendChild(mensageError);
+}
+const main = async () => {
+  createLoading();
+  try {
+    const produtos = await fetchProductsList('computador');
+    produtos.forEach((produto) => {
+      const { id, title, thumbnail, price } = produto;
+      const product = createProductElement({ id, title, thumbnail, price });
+      productList.appendChild(product);
+    });
+  } catch (error) {
+    errorMessage();
+  } finally {
+    removeLoading();
+  }
+};
+main();
