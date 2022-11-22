6d478e5ccfa0ec8d893c8fb63d7e5c43669be106
iriscacais <mandeparairis@gmail.com>
2022-11-22 18:23:12 -0300

    requisito 11

--- src/helpers/cepFunctions.js
+++ src/helpers/cepFunctions.js
@@ -1,7 +1,26 @@
-export const getAddress = () => {
-  // seu código aqui
+export const getAddress = async (cep) => {
+  const spanCep = document.querySelector('.cart__address');
+  const CEP1 = `https://cep.awesomeapi.com.br/json/${cep}`;
+  const CEP2 = `https://brasilapi.com.br/api/cep/v2/${cep}`;
+  try {
+    const result = await Promise.any([fetch(CEP1), fetch(CEP2)]);
+    const data = await result.json();
+    return data;
+  } catch {
+    spanCep.innerHTML = 'CEP não encontrado';
+  }
 };
 
-export const searchCep = () => {
-  // seu código aqui
+export const searchCep = async () => {
+  const inputCep = document.querySelector('.cep-input');
+  const spanCep = document.querySelector('.cart__address');
+
+  const data = await getAddress(inputCep.value);
+  const tamanhoCep = 8;
+  if (!Number(inputCep.value) || inputCep.value.length !== tamanhoCep) {
+    spanCep.innerHTML = 'CEP não encontrado';
+  } else {
+    spanCep.innerHTML = `${data.address} - ${data.district} - ${data
+      .city} - ${data.state}`;
+  }
 };
